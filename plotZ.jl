5c7d4b8d1a2f92d2efa9c9fc3054e274e9526fa2
liuyang <1971443614@qq.com>
2023-08-25 13:09:03 +0800

    第三次提交

--- plotZ.jl
+++ plotZ.jl
@@ -1,5 +1,10 @@
 using Plots, LaTeXStrings
-contour(btR, btI, real(Z), levels=[0], color=[:red], legend=:none)
-contour!(btR, btI, imag(Z), levels=[0], color=[:blue], legend=:none)
-xlabel!(L"β_r ")
-ylabel!(L"β_i(π)")
+@load "F:\\julia\\IsingHOTRG\\figure\\Z.jld2" btR btI Z
+    # gr();
+    plot_size = (3500,2625);
+    contour(btR, btI, real(Z), levels=[0], seriescolor=:reds, legend=:none, size=plot_size)
+    contour!(btR, btI, imag(Z), levels=[0], seriescolor=:blues, legend=:none, size=plot_size)
+    xlabel!(L"β_r ")
+    ylabel!(L"β_i(π)")
+    #save plot as PNG
+    savefig("F:\\julia\\IsingHOTRG\\figure\\z.png")
